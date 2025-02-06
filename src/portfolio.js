4bdae3263768eafe50ce41bb4f938eab915e160f
pharmaboy1 <51694293+pharmaboy1@users.noreply.github.com>
2025-02-07 00:58:01 +0500

    added

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Tufail Ahmad",
   title: "Hi all, I'm Tufail",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Pharmacist."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
